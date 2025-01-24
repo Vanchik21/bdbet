8f5cd81472978441832976c9d240fa066355dc96
Vanchik21 <ipz221_zia@student.ztu.edu.ua>
2025-01-24 07:18:35 +0200

    finaly project

---
+++ bdbet/DataBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlClient;
+
+namespace bdbet
+{
+    public class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-R9DJHB9;Initial Catalog=bdbet;Integrated Security=True");
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+
+}
